f266e73e496b897fcda21a673637c00c9556a1a9
Clement Hoang <clement.hoang24@gmail.com>
2015-10-10 14:28:33 -0700

    Adds nice landing page

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import {TextField, RaisedButton} from 'material-ui';
 
 window.React = React;
 
 class App extends Component {
 	render() {
-		return <span>Hello World</span>;
+		return (
+			<div style={{width: '100%', height: '100%', position: 'absolute'}}>
+				<div style={{top: '40%', textAlign: 'center', position: 'relative'}}>
+					<TextField
+					  hintText="Display Name"
+					  floatingLabelText="Enter a display name" />
+					<RaisedButton label="Join Game" primary={true} />
+				</div>
+			</div>
+		);
 	}
 };
 
