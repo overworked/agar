cb3eea4c145ab5ad34fca16d40db91838a3d604f
Andy Tu <andysctu@gmail.com>
2015-10-10 20:41:52 -0700

    hi

--- js/map.js
+++ js/map.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import superagent from 'superagent';
+import jsonp from 'superagent-jsonp';
 
 nokia.Settings.set('app_id', 'gg7K9ZNHGDxtTjp04GO7');
 nokia.Settings.set('app_code', 'y5vrY3rXNdVVd9eT_G1K7w');
@@ -72,29 +74,80 @@ export default class Map extends Component {
 }
 
 function sendSelfLocation() {
-	// console.log(debug++);
-	// if (debug > 100) debug = 0;
-	httpGetAsync("https://prod-mmx-001.magnet.com:5221/mmxmgmt/api/v1/topics/agar.me/items?size=200", sendSelfCallback())
+
 }
 
-function httpGetAsync(theUrl, callback)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    xmlHttp.setRequestHeader("x-mmx-app-id", "7yyifl20zg2")
-    xmlHttp.setRequestHeader("x-mmx-api-key", "f6526236-eac9-4f17-ae53-81ca2564840a")
-    xmlHttp.send(null);
+function sendSelfCallback(err, xml){
+	
 }
 
-function sendSelfCallback(xml){
-	console.log(xml)
-}
 
 function retrieveOthersLocations(){
-	console.log(debug2--);
-	if (debug2 < -100) debug2 = 0;
+    // var xmlHttp = new XMLHttpRequest();
+    // // xmlHttp.onreadystatechange = function() { 
+    // //     if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+    // //         callback(xmlHttp.responseText);
+    // // }
+    // xmlHttp.open("GET", "https:prod-mmx-001.magnet.com:5221/mmxmgmt/api/v1/topics/agar.me/items?size=200", true); // true for asynchronous
+    // xmlHttp.setRequestHeader("X-mmx-app-id", "7yyifl20zg2")
+    // xmlHttp.setRequestHeader("X-mmx-api-key", "f6526236-eac9-4f17-ae53-81ca2564840a")
+    // xmlHttp.send(callback);
+
+    var url = 'http://jsonp.afeld.me/?url=https://prod-mmx-001.magnet.com:5221/mmxmgmt/api/v1/topics/agar.me/items&size=200';
+	
+	var xhr = createCORSRequest('GET', url);
+
+	xhr.onload = function() {
+		console.log(xhr.responseText);
+	}
+
+	xhr.setRequestHeader("X-mmx-app-id", "7yyifl20zg2");
+	xhr.setRequestHeader("X-mmx-api-key", "f6526236-eac9-4f17-ae53-81ca2564840a")
+	xhr.send();
 }
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
+
+  } else if (typeof XDomainRequest != "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
+  }
+  return xhr;
+}
+
+	// // // console.log(debug++);
+	// // // if (debug > 100) debug = 0;
+	// superagent.get('http://crossorigin.me/https://prod-mmx-001.magnet.com:5221/mmxmgmt/api/v1/topics/agar.me/items?size=200')
+	// .set("X-mmx-app-id", "7yyifl20zg2")
+	// .set("X-mmx-api-key", "f6526236-eac9-4f17-ae53-81ca2564840a")
+	// .set("Accept", "application/json")
+	// .use(jsonp)
+	// .end(retrieveOthersCallback);//retrieveOthersCallback);
+	// // // var req = new XMLHttpRequest();
+
+	// // req.open('GET', 'https://prod-mmx-001.magnet.com:5221/mmxmgmt/api/v1/topics/agar.me/items?size=200', true);
+ // //    // Just like regular ol' XHR
+ // //    req.onreadystatechange = function() {
+ // //        if (req.readyState === 4) {
+ // //            if (req.status >= 200 && req.status < 400) {
+ // //                // JSON.parse(req.responseText) etc.
+ // //            } else {
+ // //                // Handle error case
+ // //            }
+ // //        }
+ // //    };
+ // //    req.send();
